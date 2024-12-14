217dff7ba646bd052b51615bf8f3270d4968aa48
sony <sony@niceday.io>
2024-12-14 21:17:54 +0900

    주문 서비스 모듈 생성2 / BaseEntity 및 AggregateRoot 공통 클래스 생성

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,10 @@
+package com.food.ordering.system.domain.entity;
+
+/**
+ * @since       2024.12.14
+ * @author      sony
+ * @description
+ **********************************************************************************************************************/
+public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
+
+}
