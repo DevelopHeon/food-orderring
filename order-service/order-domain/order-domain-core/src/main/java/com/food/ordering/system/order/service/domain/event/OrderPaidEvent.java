3b7a7029479d8f4b7c500d7e95fdef2653890f62
sony <sony@niceday.io>
2024-12-14 23:29:46 +0900

    주문 서비스 도메인 계층에서 도메인 이벤트 구현

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderPaidEvent.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.order.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author sony
+ * @description
+ * @since 2024.12.14
+ **********************************************************************************************************************/
+public class OrderPaidEvent extends OrderEvent {
+
+    public OrderPaidEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
