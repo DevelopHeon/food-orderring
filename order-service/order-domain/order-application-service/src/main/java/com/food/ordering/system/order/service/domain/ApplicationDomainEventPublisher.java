302ef15f3d607e463e8f52d9df26d62cd98d2ec2
sony <sony@niceday.io>
2024-12-17 00:45:51 +0900

    Publish with transactional listener

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ApplicationDomainEventPublisher.java
@@ -0,0 +1,35 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author sony
+ * @description
+ * @since 2024.12.17
+ **********************************************************************************************************************/
+@Slf4j
+@Component
+public class ApplicationDomainEventPublisher implements ApplicationEventPublisherAware, DomainEventPublisher<OrderCreatedEvent> {
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    public ApplicationDomainEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void publish(OrderCreatedEvent domainEvent) {
+        this.applicationEventPublisher.publishEvent(domainEvent);
+        log.info("OrderCreatedEvent is published for order id: {}", domainEvent.getOrder().getId().getValue());
+    }
+}
