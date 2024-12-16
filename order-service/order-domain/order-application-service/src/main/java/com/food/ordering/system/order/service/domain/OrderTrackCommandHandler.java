4c700a7ddf190db393eec9541aeb1707b90e9050
DevelopHeon <gmlgjs11@naver.com>
2024-12-17 00:28:42 +0900

    입력 포트 구현

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,20 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author sony
+ * @description
+ * @since 2024.12.17
+ **********************************************************************************************************************/
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+
+    }
+}
