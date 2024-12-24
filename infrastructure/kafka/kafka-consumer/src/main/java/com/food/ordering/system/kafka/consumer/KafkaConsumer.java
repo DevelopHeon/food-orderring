de5c1d2cda9763fa3c402058d797103807ba4c89
DevelopHeon <gmlgjs11@naver.com>
2024-12-24 18:31:10 +0900

    kafka 소비자 일반 모듈 구현

---
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.kafka.consumer;
+
+import org.apache.avro.specific.SpecificRecordBase;
+
+import java.util.List;
+
+/**
+ * @author sony
+ * @description
+ * @since 2024.12.24
+ **********************************************************************************************************************/
+public interface KafkaConsumer<T extends SpecificRecordBase> {
+    void receive(List<T> message,List<Long> keys, List<Integer> partitions, List<Long> offsets);
+}
