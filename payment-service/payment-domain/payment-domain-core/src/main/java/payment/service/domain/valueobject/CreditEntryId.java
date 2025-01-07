09ec0c1be1b7551f8f08246904109a5b2faf754d
sony <sony@niceday.io>
2025-01-07 23:58:53 +0900

    결제 서비스 - 도메인 코어 모듈 집계 루트, 엔티티 및 값 개체 구현

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/payment/service/domain/valueobject/CreditEntryId.java
@@ -0,0 +1,17 @@
+package payment.service.domain.valueobject;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+/**
+ * @author sony
+ * @description
+ * @since 2025.01.07
+ **********************************************************************************************************************/
+public class CreditEntryId extends BaseId<UUID> {
+
+    public CreditEntryId(UUID value) {
+        super(value);
+    }
+}
