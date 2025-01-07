4a159b812922bccac8cba8dcd97bcebf92d51b86
sony <sony@niceday.io>
2025-01-08 00:02:24 +0900

    결제 서비스 - 도메인 코어 모듈 예외 및 도메인 이벤트 구현

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/payment/service/domain/event/PaymentCancelledEvent.java
@@ -0,0 +1,18 @@
+package payment.service.domain.event;
+
+import payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * @author sony
+ * @description
+ * @since 2025.01.08
+ **********************************************************************************************************************/
+public class PaymentCancelledEvent extends PaymentEvent {
+
+    public PaymentCancelledEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/payment/service/domain/event/PaymentEvent.java
@@ -0,0 +1,37 @@
+package payment.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * @author sony
+ * @description
+ * @since 2025.01.07
+ **********************************************************************************************************************/
+public abstract class PaymentEvent implements DomainEvent<Payment> {
+
+    private final Payment payment;
+    private final ZonedDateTime createdAt;
+    private final List<String> failureMessages;
+
+    public PaymentEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        this.payment = payment;
+        this.createdAt = createdAt;
+        this.failureMessages = failureMessages;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public List<String> getFailureMessages() {
+        return failureMessages;
+    }
+}
