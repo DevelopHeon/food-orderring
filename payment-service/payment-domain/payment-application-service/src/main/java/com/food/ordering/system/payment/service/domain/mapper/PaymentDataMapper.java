1e85b0bafc805d84debfb821473e0aedd675759f
sony <sony@niceday.io>
2025-01-08 23:53:16 +0900

    결제 서비스 - 애플리케이션 서비스 도메인 모듈 mapper, config 및 ports 구현

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.payment.service.domain.mapper;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author sony
+ * @description
+ * @since 2025.01.08
+ **********************************************************************************************************************/
+@Component
+public class PaymentDataMapper {
+
+}
