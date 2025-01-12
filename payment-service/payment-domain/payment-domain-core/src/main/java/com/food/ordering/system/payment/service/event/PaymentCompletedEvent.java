b7bb484d3afe72efe49c7ff78646b98aef77934b
sony <sony@niceday.io>
2025-01-12 19:13:07 +0900

    결제 서비스 - 데이터 액세스 모듈 구현

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/event/PaymentCompletedEvent.java
@@ -0,0 +1,28 @@
+package com.food.ordering.system.payment.service.event;
+
+import com.food.ordering.system.payment.service.entity.Payment;
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+
+/**
+ * @author sony
+ * @description
+ * @since 2025.01.08
+ **********************************************************************************************************************/
+public class PaymentCompletedEvent extends PaymentEvent{
+
+    private final DomainEventPublisher<PaymentCompletedEvent> paymentCompletedEventDomainEventPublisher;
+    public PaymentCompletedEvent(Payment payment,
+                                 ZonedDateTime createdAt,
+                                 DomainEventPublisher<PaymentCompletedEvent> paymentCompletedEventDomainEventPublisher) {
+        super(payment, createdAt, Collections.emptyList());
+        this.paymentCompletedEventDomainEventPublisher = paymentCompletedEventDomainEventPublisher;
+    }
+
+    @Override
+    public void fire() {
+        paymentCompletedEventDomainEventPublisher.publish(this);
+    }
+}
