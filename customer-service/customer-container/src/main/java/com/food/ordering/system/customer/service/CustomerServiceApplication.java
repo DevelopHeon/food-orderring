d1806397c39cfc058e6a855c19f31f743e7527dd
DevelopHeon <gmlgjs11@naver.com>
2025-02-19 23:51:39 +0900

    고객 서비스 모듈 구현

--- customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
+++ customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-/**
- * @author sony
- * @description
- * @since 2025.01.07
- **********************************************************************************************************************/
-@EnableJpaRepositories(basePackages = {"com.food.ordering.system.customer.service.dataaccess", "com.food.ordering.system.dataaccess"})
+
+@EnableJpaRepositories(basePackages = { "com.food.ordering.system.customer.service.dataaccess", "com.food.ordering.system.dataaccess"})
 @EntityScan(basePackages = { "com.food.ordering.system.customer.service.dataaccess", "com.food.ordering.system.dataaccess" })
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class CustomerServiceApplication {
